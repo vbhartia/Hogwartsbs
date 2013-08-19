ee68629fd1de401f100b8ae71057094ae147d714
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-08-19 10:11:14 -0400

    fixed authenticate

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,7 +10,7 @@ class UserController < ApplicationController
 		course_count = current_user.courses.count
 
 		if (course_count < 5)
-			flash.alert = 'Sorry, but in order to view the courses that this person is enrolled in you, must add your own courses'
+			flash.alert = 'Sorry, but in order to view the courses that this person is enrolled in you, must add atleast 5 of your own courses'
 			redirect_to my_courses_path
 		else
 			@user = User.find(params[:username])		
